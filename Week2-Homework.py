38501a5b24bf263f7cd63bd03939e2637f9d3c2f
leukothea <leukothea@gmail.com>
2013-11-10 21:16:36 -0800

    Attempt at the grid-building problem... not done.

---
+++ Week2-Homework.py
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+# Name:        Week2Homework2
+# Purpose:     Attempt at the second chunk of homework due 2013-10-08
+# Author:      Catherine Warren
+# Created:     2013-10-07
+#-------------------------------------------------------------------------------
+
+def double():
+    f()
+    f()
+
+def Log():
+    print('+ - - - - '),
+
+def Pillar():
+    print('|         |        '),
+
+double(Log)
+
+"""
+def TwoLogs():
+    double(Log)
+    print('+')
+
+def TwoPillars():
+    double(Pillar)
+    print('|')
+
+def Row():
+    TwoLogs()
+    TwoPillars()
+    TwoPillars()
+
+def print_grid():
+    Row()
+    Row()
+    Log()
+
+print_grid()
+"""
