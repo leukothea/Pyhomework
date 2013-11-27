372d5914725b2e472c222fe31c145a7e9d1a7f09
leukothea <leukothea@gmail.com>
2013-11-27 15:48:19 -0800

    Update RGFTC-0.01.py

--- RGFTC-0.01.py
+++ RGFTC-0.01.py
@@ -28,17 +28,26 @@ class Card(object):
 	# to hold the 25 cards 
 	# Each card is denoted by a letter, to make later comparison between cards easier (we will need to determine
 	# which card comes "first"). 
-	# Tuple values are entered on each card in order from lowest to highest number.
+	# Tuple values are entered on each card in order from lowest number going clockwise around the card.
 	a = ("Rose", "Bread", "Demons", "Fish")
 	b = ("House", "Birds", "Fish", "Ring")
 	c = ("Rose", "Demons", "House", "Letter")
 	d = ("Bear", "Letter", "Ring", "Bread")
 
-# -- Processing -- #
+	# def __getitem__(Card):
+	# maybe getitem will be useful: operator.__getitem__(a, b) - Return the value of a at index b.
+	# Standard operators as functions: http://docs.python.org/2/library/operator.html#operator.getitem
+
+
 
 def PictureMeaning(numberofcards):
-	# right now this is just to return a random single-picture reading so I know that part works
+# right now this is just to return a random single-picture reading so I know that part works
 	for name, meaning in random.sample(Meaning.items(), numberofcards):
-            print ("{lhs} : {rhs}".format(lhs=name, rhs=meaning))
+		print ("{lhs} : {rhs}".format(lhs=name, rhs=meaning))
+
+			
+# -- Processing -- #
+
 
 print PictureMeaning(1)
+
